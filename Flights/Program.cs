c02de0bdeb2ec71219c441f44d257d71115e1cef
furiax <carlmalfliet@proximus.be>
2023-09-19 10:42:35 +0200

    added ng-openapi-gen to connect our asp api with the angular

--- Flights/Program.cs
+++ Flights/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c =>
+{
+	c.AddServer(new OpenApiServer
+	{
+		Description = "Development Server",
+		Url = "https://localhost:7132"
+	});
+});
 
 var app = builder.Build();
+
+app.UseCors(builder => builder.WithOrigins("*"));
 
 app.UseSwagger().UseSwaggerUI();
 // Configure the HTTP request pipeline.
