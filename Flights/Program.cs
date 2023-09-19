2ad68926c6c36bd78a7caeb65cb90c9030ca3824
furiax <carlmalfliet@proximus.be>
2023-09-19 10:24:13 +0200

    create the readmodels and fill them with some test data

--- Flights/Program.cs
+++ Flights/Program.cs
@@ -3,9 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger().UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
