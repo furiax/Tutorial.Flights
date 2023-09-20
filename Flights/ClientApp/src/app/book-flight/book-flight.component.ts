e38689c680973ad9102397d43c5fb882966ff310
furiax <carlmalfliet@proximus.be>
2023-09-20 09:16:54 +0200

    create the register passenger page and made the htlm template

--- Flights/ClientApp/src/app/book-flight/book-flight.component.ts
+++ Flights/ClientApp/src/app/book-flight/book-flight.component.ts
@@ -8,7 +8,8 @@ import { FlightRm } from '../api/models';
   templateUrl: './book-flight.component.html',
   styleUrls: ['./book-flight.component.css']
 })
-export class BookFlightComponent implements OnInit{
+export class BookFlightComponent implements OnInit {
+
   constructor(private route: ActivatedRoute,
     private router: Router,
     private flightService: FlightService) { }
@@ -17,22 +18,28 @@ export class BookFlightComponent implements OnInit{
   flight: FlightRm = {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(p => this.findFlight (p.get("flightId")))
+    this.route.paramMap
+      .subscribe(p => this.findFlight(p.get("flightId")))
   }
 
   private findFlight = (flightId: string | null) => {
     this.flightId = flightId ?? 'not passed';
 
     this.flightService.findFlight({ id: this.flightId })
-      .subscribe(flight => this.flight = flight, this.handleError)
+      .subscribe(flight => this.flight = flight,
+        this.handleError)
   }
 
-  private handleError(err: any) {
+  private handleError = (err: any) => {
+
     if (err.status == 404) {
-      alert("Flight not found!")
+      alert('Flight not found!')
+      this.router.navigate(['/search-flights'])
     }
+
     console.log("Response Error. Status: ", err.status)
     console.log("Response Error. Status Text: ", err.statusText)
     console.log(err)
   }
+
 }
