ee68b860a81409246b47d12fb15c28103f762c9b
furiax <carlmalfliet@proximus.be>
2023-09-20 10:14:26 +0200

    created a passengers list to store the form info

--- Flights/Controllers/PassengerController.cs
+++ Flights/Controllers/PassengerController.cs
@@ -8,13 +8,18 @@ namespace Flights.Controllers
 	[ApiController]
 	public class PassengerController : ControllerBase
 	{
+		static private IList<NewPassengerDto> Passengers = new List<NewPassengerDto>();
+
+
 		[HttpPost]
 		[ProducesResponseType(201)]
 		[ProducesResponseType(400)]
 		[ProducesResponseType(500)]
 		public IActionResult Register(NewPassengerDto dto)
 		{
-			throw new NotImplementedException();
+			Passengers.Add(dto);
+			System.Diagnostics.Debug.WriteLine(Passengers.Count);
+			return Ok();
 		}
 	}
 }
