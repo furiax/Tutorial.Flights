338f93f7935da71deb45840193599392f650dd1b
furiax <carlmalfliet@proximus.be>
2023-09-20 09:49:15 +0200

    implemented reactive form binding on registerPassengers

--- Flights/ClientApp/src/app/register-passenger/register-passenger.component.ts
+++ Flights/ClientApp/src/app/register-passenger/register-passenger.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PassengerService } from './../api/services/passenger.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register-passenger',
   templateUrl: './register-passenger.component.html',
   styleUrls: ['./register-passenger.component.css']
 })
-export class RegisterPassengerComponent {
+export class RegisterPassengerComponent implements OnInit {
+
+  constructor(private passengerService: PassengerService, private fb: FormBuilder) { }
+
+  form = this.fb.group({
+    email: [''],
+    firstName: [''],
+    lastName: [''],
+    isFemale: [true]
+  })
+
+  ngOnInit(): void { }
+
+  register() {
+    console.log("Form Values:", this.form.value);
+    this.passengerService.registerPassenger();
+  }
 
 }
---
+++ Flights/Controllers/PassengerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Flights.Dtos;
+
+namespace Flights.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class PassengerController : ControllerBase
+	{
+		[HttpPost]
+		[ProducesResponseType(201)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(500)]
+		public IActionResult Register(NewPassengerDto dto)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
