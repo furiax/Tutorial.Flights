b2e9d98d1dea88b77a20a1197c0a83721fb8e28b
furiax <carlmalfliet@proximus.be>
2023-09-21 19:37:05 +0200

    created flight and timeplace entities

---
+++ Flights/Domain/Entities/Flight.cs
@@ -0,0 +1,10 @@
+﻿namespace Flights.Domain.Entities
+{ 
+	public record Flight(
+		Guid Id,
+		string Airline,
+		string Price,
+		TimePlace Departure,
+		TimePlace Arrival,
+		int RemainingNumberOfSeats);
+}
