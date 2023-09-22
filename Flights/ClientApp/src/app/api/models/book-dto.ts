14ad4fc28cd5326d3e5395bd1eb97045a7549d61
furiax <carlmalfliet@proximus.be>
2023-09-22 14:07:59 +0200

    inserted the values in my-bookings.html added the necessary code in my-bookings.ts created a shortcut in the navmenu to my-bookings

--- Flights/ClientApp/src/app/api/models/book-dto.ts
+++ Flights/ClientApp/src/app/api/models/book-dto.ts
@@ -2,6 +2,6 @@
 /* eslint-disable */
 export interface BookDto {
   flightId?: string;
-  numberOfSeats?: null |number;
+  numberOfSeats?: null | number;
   passengerEmail?: null | string;
 }
--- Flights/ClientApp/src/app/my-bookings/my-bookings.component.ts
+++ Flights/ClientApp/src/app/my-bookings/my-bookings.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookingRm } from '../api/models';
+import { BookingService } from './../api/services/booking.service';
+import { AuthService } from './../auth/auth.service';
 
 @Component({
   selector: 'app-my-bookings',
   templateUrl: './my-bookings.component.html',
   styleUrls: ['./my-bookings.component.css']
 })
-export class MyBookingsComponent {
+export class MyBookingsComponent implements OnInit {
+  bookings!: BookingRm[];
+  constructor(private bookingService: BookingService,
+  private authService : AuthService) { }
 
+  ngOnInit(): void {
+    this.bookingService.listBooking({ email: this.authService.currentUser?.email ?? '' })
+      .subscribe(r => this.bookings = r, this.handleError);
+  }
+
+  private handleError(err: any) {
+    console.log("Response Error, Status:", err.status);
+    console.log("Response Error, Status Text:", err.statusText);
+    console.log(err);
+  }
 }
