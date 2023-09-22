3eeaec6c6bd736005f95fb58a7e46706aca81d85
furiax <carlmalfliet@proximus.be>
2023-09-22 13:11:15 +0200

    styled the my bookings page created a booking controller created a booking read model

---
+++ Flights/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flights.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class BookingController : ControllerBase
+	{
+
+	}
+}
---
+++ Flights/ReadModels/BookingRm.cs
@@ -0,0 +1,15 @@
+﻿using Flights.Domain.Entities;
+
+namespace Flights.ReadModels
+{
+	public record BookingRm
+	(
+		Guid FlightId,
+		string Airline,
+		string Price,
+		TimePlace Arrival,
+		TimePlace Departure,
+		int NumberOfBookedSeats,
+		string PassengerEmail
+		);
+}
