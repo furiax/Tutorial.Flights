b475d996d66ae87b39f0b5ce3dbe1f32f78731de
furiax <carlmalfliet@proximus.be>
2023-09-22 13:15:27 +0200

    adding the endpoint to the bookingcontroller

--- Flights/Controllers/BookingController.cs
+++ Flights/Controllers/BookingController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Flights.Data;
+using Flights.ReadModels;
 
 namespace Flights.Controllers
 {
@@ -7,6 +9,19 @@ namespace Flights.Controllers
 	[Route("[controller]")]
 	public class BookingController : ControllerBase
 	{
+		private readonly Entities _entities;
+        public BookingController(Entities entities)
+        {
+            _entities = entities;
+        }
 
-	}
+		[HttpGet("{email}")]
+		[ProducesResponseType(500)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(typeof(IEnumerable<BookingRm>),200)]
+		public ActionResult<IEnumerable<BookingRm>> List(string email)
+		{
+
+		}
+    }
 }
